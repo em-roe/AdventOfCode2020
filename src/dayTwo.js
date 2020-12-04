259cd2b72e46faf0b8780e23e6855ef65dcb027d
roecodes <eroe@eroe.local>
2020-12-04 11:47:12 -0500

    day two part 2

--- src/dayTwo.js
+++ src/dayTwo.js
@@ -5,17 +5,43 @@ const execute = (input) => {
 
   let validPasswords = 0;
 
+  // PART ONE
+  //   for (let i = 0; i < data.length; i++) {
+  //     const currentEntry = data[i].split(' ');
+  //     const frequency = currentEntry[0].split('-');
+  //     const letter = currentEntry[1].charAt(0);
+  //     const entry = currentEntry[2];
+  //     const regex = new RegExp(`[^${letter}]`, 'g');
+  //     const removeOtherLetters = entry.replace(regex, '');
+
+  //     if (
+  //       removeOtherLetters.length >= frequency[0] &&
+  //       removeOtherLetters.length <= frequency[1]
+  //     ) {
+  //       validPasswords++;
+  //     }
+  //   }
+  //   console.log(validPasswords);
+
+  // PART TWO
   for (let i = 0; i < data.length; i++) {
     const currentEntry = data[i].split(' ');
     const frequency = currentEntry[0].split('-');
+
+    const positionA = parseInt(frequency[0]) - 1;
+    const positionB = parseInt(frequency[1]) - 1;
+
     const letter = currentEntry[1].charAt(0);
+
     const entry = currentEntry[2];
-    const regex = new RegExp(`[^${letter}]`, 'g');
-    const removeOtherLetters = entry.replace(regex, '');
+
+    const first = entry.charAt(positionA);
+
+    const second = entry.charAt(positionB);
 
     if (
-      removeOtherLetters.length >= frequency[0] &&
-      removeOtherLetters.length <= frequency[1]
+      (first === letter && second !== letter) ||
+      (first !== letter && second === letter)
     ) {
       validPasswords++;
     }
